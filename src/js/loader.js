a6b6746cbc2464628a55dff43c2008bf0e43f1b6
cashoutlen <104978695+cashoutlen@users.noreply.github.com>
2022-05-06 15:17:13 +0100

    Delete loader.js

--- src/js/loader.js
+++
@@ -1,9 +0,0 @@
-document.title = collectionInfo.name;
-document.getElementById("myVideo").src = `./assets/${collectionInfo.medias.preview}`;
-
-
-document.getElementById("h2").innerText = collectionInfo.name;
-document.getElementById("message").innerText = collectionInfo.text1;
-document.getElementById("message2").innerText = collectionInfo.text2;
-
-document.body.style.backgroundColor = collectionInfo.background.color
